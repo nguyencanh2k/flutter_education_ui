74ed0e145abe82ba50d56a5308a2c9837998b4d8
nguyencanh2k <nguyencanh932000@gmail.com>
2023-07-25 22:35:45 +0700

    first commit

---
+++ lib/widgets/description_section.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class DescriptionSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          style: TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.7)),
+          textAlign: TextAlign.justify,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            Text(
+              "Course Length: ",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600),
+            ),
+            Icon(
+              Icons.timer,
+              color: Color(0xFF674AEF),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "26 Hours",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Text(
+              "Rating: ",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "4.5",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
